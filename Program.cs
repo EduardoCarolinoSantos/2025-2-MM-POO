934d3a6c0ecc280ac2c763c383935162f3a65296
Eduardo C Santos <161091487+EduardoCarolinoSantos@users.noreply.github.com>
2025-09-01 23:24:20 +0000

    Primeiro Código cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite sua idade: ");
+        string input = Console.ReadLine(); // lê o texto que o usuário digitou
+
+        // Convertendo o texto para número inteiro
+        int idade;
+        bool sucesso = int.TryParse(input, out idade);
+
+        if (!sucesso)
+        {
+            Console.WriteLine("Por favor, digite um número válido.");
+            return; // encerra o programa
+        }
+
+        if (idade > 2)
+        {
+            Console.WriteLine("Você é adulto");
+        }
+        else if (idade < 2)
+        {
+            Console.WriteLine(" Léo você é potudo.");
+        }
+        else
+        {
+            Console.WriteLine("Você tem exatamente 2 anos");
+        }
+    }
+}
