0a7dca68bb6047fa89de37faf1731c44d3d5c8e5
Eduardo C Santos <161091487+EduardoCarolinoSantos@users.noreply.github.com>
2025-09-02 00:31:48 +0000

    Aula --> 2

--- Program.cs
+++ Program.cs
@@ -1,33 +1,5 @@
-﻿using System;
+﻿Console.WriteLine("Hello, Word!");
 
-class Program
-{
-    static void Main()
-    {
-        Console.Write("Digite sua idade: ");
-        string input = Console.ReadLine(); // lê o texto que o usuário digitou
+Televisao tv = new Televisao(55f);
 
-        // Convertendo o texto para número inteiro
-        int idade;
-        bool sucesso = int.TryParse(input, out idade);
-
-        if (!sucesso)
-        {
-            Console.WriteLine("Por favor, digite um número válido.");
-            return; // encerra o programa
-        }
-
-        if (idade > 2)
-        {
-            Console.WriteLine("Você é adulto");
-        }
-        else if (idade < 2)
-        {
-            Console.WriteLine(" Léo você é potudo.");
-        }
-        else
-        {
-            Console.WriteLine("Você tem exatamente 2 anos");
-        }
-    }
-}
+Console.WriteLine($"A tv tem o tamanho {tv.Tamanho}");
